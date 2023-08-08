6a190ae9e9d7199edc2ec5881fc0c0c45ce37f25
Romina Almada <cra.romialmada@gmail.com>
2023-08-08 18:47:19 -0300

    first commit 0808

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,62 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    //Atributos o propiedades
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    //Constructores
+
+    public Client(){
+
+    }
+    public Client(String first, String last, String mail) {
+        firstName = first;
+        lastName = last;
+        email = mail;
+    }
+
+    //Métodos o comportamientos
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = lastName;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
